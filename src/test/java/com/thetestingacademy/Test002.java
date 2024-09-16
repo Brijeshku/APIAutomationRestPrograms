9d483296caac4e00379d2ec94990f3ca60efd946
Brijesh Kumar <bkumar9983@yahoo.in>
2024-09-17 01:00:28 +0530

    files are added

--- src/test/java/com/thetestingacademy/Test002.java
+++ src/test/java/com/thetestingacademy/Test002.java
@@ -1,4 +1,27 @@
 package com.thetestingacademy;
 
+import io.restassured.RestAssured;
+
 public class Test002 {
+
+    public static void main(String[] args) {
+        System.out.println("Rest assured test case");
+        System.out.println("Get request demo");
+
+
+//        given() // prescripts / headers, url , parameters , body
+//        when()  // HTTP method , like , get,put, post ,delete..
+//        then() // verify the response ER==AR
+
+        RestAssured
+                .given()
+        .baseUri("https://restful-booker.herokuapp.com/booking")
+        .basePath("/bookingid/1").log().all()
+                .when()
+                .get()
+                .then().log().all()
+                .statusCode(404);
+
+//
+    }
 }
